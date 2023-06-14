22570e43f109e6d0897806e73e69c7c9fd5e78dc
Mariga <mariga.bn@gmail.com>
2023-06-14 14:12:07 -0700

    Data Structures II

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
-!/usr/bin/python3
-def uniq_add(my_list=[])
-    unique_integers = set()
+#!/usr/bin/python3
 
-    for num in my_list:
-        unique_integers.add(num)
-
-    total = sum(unique_integers)
-
-    return total
+def uniq_add(my_list=[]):
+    my_set = set(my_list)
+    number_list = list(my_set)
+    result = sum(my_set)
+    return result
