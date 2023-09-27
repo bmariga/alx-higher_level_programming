ff2e73f0de5048efb18ead168ff7727effc316d2
Mariga Bruce <mariga.bn@gmail.com>
2023-09-27 15:16:26 +0300

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-if (parseInt(process.argv[2])) {
-  console.log("My number: " + parseInt(process.argv[2]));
+const process = require("process");
+const firstArg = process.argv[2];
+const convertedFirstArg = parseInt(firstArg);
+
+if (!isNaN(convertedFirstArg)) {
+  console.log("My number:", convertedFirstArg);
 } else {
   console.log("Not a number");
 }
