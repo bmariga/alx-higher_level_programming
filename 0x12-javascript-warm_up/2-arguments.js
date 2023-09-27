c9d936d8d38a6527c14fac37fc2c76ee187bc80e
Mariga Bruce <mariga.bn@gmail.com>
2023-09-27 15:15:35 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv[3]) {
-  console.log("Arguments found");
-} else if (process.argv[2]) {
+const argumentsCount = process.argv.length - 2;
+
+if (argumentsCount === 0) {
+  console.log("No argument");
+} else if (argumentsCount === 1) {
   console.log("Argument found");
 } else {
-  console.log("No argument");
+  console.log("Arguments found");
 }
