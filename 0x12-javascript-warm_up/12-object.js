c701ea9fd95bec833df595d9151dbf6c8d123f9a
Mariga Bruce <mariga.bn@gmail.com>
2023-09-27 15:38:43 +0300

    Object

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-// Update this script to replace the value 12 with 89
-
 const myObject = {
   type: "object",
   value: 12,
 };
+
 console.log(myObject);
+
+// Replace the value 12 with 89
 myObject.value = 89;
+
 console.log(myObject);
